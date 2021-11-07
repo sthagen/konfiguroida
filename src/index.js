c6b1e194a0694ed0208bc9158e9b1909e68227dc
Stefan Hagen <stefan@hagen.link>
2021-11-07 13:13:39 +0100

    Refactoring and implementation of validated cooking mode (WIP)

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ const partial = (func, first) => {
 }
 
 /**
- * Returns an function that creates a pair object.
+ * Returns a function that creates a pair object.
  * @param {string} first - key mapping to the first positional value.
  * @param {string} second - key mapping to the second and last positional value.
  */
-const pairZip = (first, second) => {
+const zip_pair = (first, second) => {
   return (left, right) => ({[first]: left, [second]: right})
 }
 
@@ -24,19 +24,41 @@ const pairZip = (first, second) => {
  * @param {string} sample - key mapping to the sample from the population.
  * @param {string} feature - key mapping to the feature variations.
  * @param {array} pairs - pairs of features and array of samples (null for final pair).
+ * @param {bool} force - (default false does / true does not) verify population use.
  */
-const cook = (population, sample, feature, pairs) => {
+const cook = (population, sample, feature, pairs, force=false) => {
+
+  if (!force
+      && (   !(population instanceof Array)
+          || !population.length
+          || !(pairs instanceof Array)
+          || !pairs.length
+      )
+  ) return undefined  // population or pairs no arrays or empty
+
   derived = [...pairs]  // Create a copy of the pairs array
 
+  if (!force) {
+    const unique = []
+    derived.forEach(pair => unique.indexOf(pair[0]) === -1
+                            ? unique.push(pair[0])
+                            : null
+    )
+    if (pairs.length !== unique.length) return undefined  // duplicate features
+  }
+
   const used = []  // registry of used population members
-  derived.forEach(pair => pair[1] ? pair[1].forEach(e => used.push(e)) : null)
+  derived.forEach(pair => pair[1]
+                          ? pair[1].forEach(e => used.push(e))
+                          : null
+  )
 
   const rest = population.filter(m => !used.includes(m))  // set difference
 
   const variation = derived[derived.length-1][0]
   derived[(derived.length - 1)] = [variation, rest]  // complete population
 
-  const make = pairZip(feature, sample)  // primed object creator
+  const make = zip_pair(feature, sample)  // primed object creator
   derived.forEach((pair, i, seq) => seq[i] = make(...pair))
 
   return derived
