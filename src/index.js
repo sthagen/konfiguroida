2c261620338f4bf9d9fee4474689fbc8c3e748c8
Stefan Hagen <stefan@hagen.link>
2021-11-07 12:11:40 +0100

    Refactored implementation and enhanced developer documentation in source

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /**
  * Returns an function with the first argument primed.
- * @param {function} func - The function the following parameter is applied to as first argument.
- * @param {string} first - The value to be fixed as first argument.
+ * @param {function} func - function to be specialized.
+ * @param {string} first - value to be fixed as first positional argument.
  */
 const partial = (func, first) => {
   return (...rest) => {
@@ -11,8 +11,8 @@ const partial = (func, first) => {
 
 /**
  * Returns an function that creates a pair object.
- * @param {string} first - The key that will map to the first positional value.
- * @param {string} second - The key that will map to the second and last positional value.
+ * @param {string} first - key mapping to the first positional value.
+ * @param {string} second - key mapping to the second and last positional value.
  */
 const pairZip = (first, second) => {
   return (left, right) => ({[first]: left, [second]: right})
@@ -20,24 +20,24 @@ const pairZip = (first, second) => {
 
 /**
  * Returns an array of objects that map population samples to distinct features.
- * @param {array} population - The array with all members of the population.
- * @param {string} sample - The key mapping to the sample from the population.
- * @param {string} feature - The key mapping to the feature variations.
- * @param {array} pairs - Pairs of features and either an array of samples or null for the last pair.
+ * @param {array} population - array with all members of the population.
+ * @param {string} sample - key mapping to the sample from the population.
+ * @param {string} feature - key mapping to the feature variations.
+ * @param {array} pairs - pairs of features and array of samples (null for final pair).
  */
 const cook = (population, sample, feature, pairs) => {
   derived = [...pairs]  // Create a copy of the pairs array
 
   const used = []  // registry of used population members
-  derived.forEach(pair => pair[1] ? pair[1].forEach(entry => used.push(entry)) : null)
+  derived.forEach(pair => pair[1] ? pair[1].forEach(e => used.push(e)) : null)
 
-  const rest = population.filter(member => !used.includes(member))  // naive set difference
+  const rest = population.filter(m => !used.includes(m))  // naive set difference
 
   const variation = derived[derived.length-1][0]
   derived[(derived.length - 1)] = [variation, rest]  // complete population
 
   const make = pairZip(feature, sample)  // primed object creator
-  derived.forEach((pair, index, seq) => seq[index] = make(...pair))
+  derived.forEach((pair, i, seq) => seq[i] = make(...pair))
 
   return derived
 }
